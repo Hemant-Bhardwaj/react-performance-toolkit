9cf90766860db366b4b844b7b5099d9614703a77
Hemant Bhardwaj <bhardwajhemant84@gmail.com>
2024-06-26 19:27:09 -0400

    setup

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+    },
+  ],
+  external: ['react'],
+  plugins: [
+    typescript(),
+    resolve(),
+    commonjs(),
+    terser(),
+  ],
+};
