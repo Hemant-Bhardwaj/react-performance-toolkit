0b2ac83b522f33a4192020820503f7b753478319
Hemant Bhardwaj <bhardwajhemant84@gmail.com>
2024-07-02 19:17:17 -0400

    create webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    library: 'reactPerformanceToolkit',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  externals: {
+    react: 'react',
+    'react-dom': 'react-dom',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
