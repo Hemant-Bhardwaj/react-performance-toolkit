0383a904438407e8626aea58f7674c281ccaefaf
Hemant Bhardwaj <bhardwajhemant84@gmail.com>
2024-07-02 19:23:27 -0400

    add error handling

--- src/codeSplitting.js
+++ src/codeSplitting.js
@@ -1,10 +1,17 @@
 import React, { Suspense } from 'react';
+import PropTypes from 'prop-types';
 
-export const withLazyLoad = (Component) => {
-  const LazyComponent = React.lazy(Component);
-  return (props) => (
+export const withLazyLoad = (importFunc) => {
+  const LazyComponent = React.lazy(importFunc);
+  const Wrapper = (props) => (
     <Suspense fallback={<div>Loading...</div>}>
       <LazyComponent {...props} />
     </Suspense>
   );
+
+  Wrapper.propTypes = {
+    fallback: PropTypes.element,
+  };
+
+  return Wrapper;
 };
--- src/memoization.js
+++ src/memoization.js
@@ -1,5 +1,14 @@
 import React, { memo, useCallback } from 'react';
+import PropTypes from 'prop-types';
 
-export const withMemo = (Component, areEqual) => memo(Component, areEqual);
+export const withMemo = (Component, areEqual) => {
+  const MemoizedComponent = memo(Component, areEqual);
+
+  MemoizedComponent.propTypes = {
+    areEqual: PropTypes.func,
+  };
+
+  return MemoizedComponent;
+};
 
 export const useSmartCallback = (callback, deps) => useCallback(callback, deps);
